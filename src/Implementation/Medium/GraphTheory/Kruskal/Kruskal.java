11587ee8e9359dc63963040e76878416303e1479
Tam Le <tle@Tams-MacBook-Pro-2.local>
2017-09-29 17:45:10 -0700

    kruskal

---
+++ src/Implementation/Medium/GraphTheory/Kruskal/Kruskal.java
@@ -0,0 +1,156 @@
+package Implementation.Medium.GraphTheory.Kruskal;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Kruskal {
+    public static void main (String[] args)
+    {
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("./resource/Kruskal/input4"));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        //Scanner in = new Scanner(System.in);
+        int V = in.nextInt();
+        int E = in.nextInt();
+
+        Graph graph = new Graph(V, E);
+
+        for(int i = 0; i < E; i++){
+            graph.edge[i].src = in.nextInt();
+            graph.edge[i].dest = in.nextInt();
+            graph.edge[i].weight = in.nextInt();
+        }
+        graph.KruskalMST();
+    }
+}
+
+class Graph {
+    int V, E;    // V-> no. of vertices & E->no.of edges
+    Edge edge[]; // collection of all edges
+
+    // Creates a graph with V vertices and E edges
+    Graph(int v, int e)
+    {
+        V = v;
+        E = e;
+        edge = new Edge[E];
+        for (int i=0; i<e; ++i)
+            edge[i] = new Edge();
+    }
+
+    // A utility function to find set of an element i
+    // (uses path compression technique)
+    int find(Subset subsets[], int i)
+    {
+        // find root and make root as parent of i (path compression)
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+
+        return subsets[i].parent;
+    }
+
+    // A function that does union of two sets of x and y
+    // (uses union by rank)
+    void union(Subset subsets[], int x, int y)
+    {
+        int xroot = find(subsets, x);
+        int yroot = find(subsets, y);
+
+        // Attach smaller rank tree under root of high rank tree
+        // (Union by Rank)
+        if (subsets[xroot].rank < subsets[yroot].rank)
+            subsets[xroot].parent = yroot;
+        else if (subsets[xroot].rank > subsets[yroot].rank)
+            subsets[yroot].parent = xroot;
+
+            // If ranks are same, then make one as root and increment
+            // its rank by one
+        else
+        {
+            subsets[yroot].parent = xroot;
+            subsets[xroot].rank++;
+        }
+    }
+
+    // The main function to construct MST using Kruskal's algorithm
+    void KruskalMST()
+    {
+        Edge result[] = new Edge[V+1];  // Tnis will store the resultant MST
+        int e = 0;  // An index variable, used for result[]
+        int i = 0;  // An index variable, used for sorted edges
+        for (i=1; i<=V; ++i)
+            result[i] = new Edge();
+
+        // Step 1:  Sort all the edges in non-decreasing order of their
+        // weight.  If we are not allowed to change the given graph, we
+        // can create a copy of array of edges
+        Arrays.sort(edge);
+
+        // Allocate memory for creating V ssubsets
+        Subset subsets[] = new Subset[V+1];
+        for(i=1; i<=V; ++i)
+            subsets[i]=new Subset();
+
+        // Create V subsets with single elements
+        for (int v = 1; v <= V; ++v)
+        {
+            subsets[v].parent = v;
+            subsets[v].rank = 0;
+        }
+
+        i = 0;  // Index used to pick next edge
+
+        // Number of edges to be taken is equal to V-1
+        while (e < V - 1)
+        {
+            // Step 2: Pick the smallest edge. And increment the index
+            // for next iteration
+            Edge next_edge = new Edge();
+            next_edge = edge[i++];
+
+            int x = find(subsets, next_edge.src);
+            int y = find(subsets, next_edge.dest);
+
+            // If including this edge does't cause cycle, include it
+            // in result and increment the index of result for next edge
+            if (x != y)
+            {
+                result[e++] = next_edge;
+                union(subsets, x, y);
+            }
+            // Else discard the next_edge
+        }
+
+        // print the contents of result[] to display the built MST
+//        System.out.println("Following are the edges in the constructed MST");
+//        for (i = 0; i < e; ++i)
+//            System.out.println(result[i].src+" -- "+result[i].dest+" == "+
+//                    result[i].weight);
+        int r = 0;
+        for (i = 0; i < e; ++i)
+            r += result[i].weight;
+        System.out.println(r);
+    }
+}
+
+class Edge implements Comparable<Edge>
+{
+    int src, dest, weight;
+
+    // Comparator function used for sorting edges based on
+    // their weight
+    public int compareTo(Edge compareEdge)
+    {
+        return this.weight-compareEdge.weight;
+    }
+};
+
+// A class to represent a subset for union-find
+class Subset
+{
+    int parent, rank;
+};
--- src/Implementation/Medium/GraphTheory/SubsetComponent/SubsetComponent.java
+++ src/Implementation/Medium/GraphTheory/SubsetComponent/SubsetComponent.java
@@ -15,16 +15,13 @@ public class SubsetComponent {
         //Scanner in = new Scanner(System.in);
         int i,j,k, result = 0;
         int n = in.nextInt();
-        long t1 = System.currentTimeMillis();
         BigInteger[] d = new BigInteger[n];
-        for(i = 0; i < n; i++){
+        for(i = 0; i < n; i++)
             d[i] = new BigInteger(in.next());
-        }
         //sample{0,1,2,3....63} is for minimize subset
         int[] sample = new int[64];
-        for(i = 0; i < 64; i++){
+        for(i = 0; i < 64; i++)
             sample[i] = i;
-        }
         //every number is represented by mask
         //ex: 36 -> 0000000000000000000000000000000000000000000000000000000000100100
         //       -> {0,1,2,3,4,2,6,7,8,9,.....63}
@@ -37,29 +34,22 @@ public class SubsetComponent {
                     if (temp == -1)
                         temp = k;
                     ds[i][k] = temp;
-                } else {
+                } else
                     ds[i][k] = k;
-                }
             }
         }
         //list out 2^n sub set
         for(i = 0; i < (1<<n); i++){
             int[] min = sample.clone();
-            for(j = 0; j < n; j++){
-                if((i & (1<<j)) > 0){
+            for(j = 0; j < n; j++)
+                if((i & (1<<j)) > 0)
                     //connect vertex together by minimize
-                    for(k = 0; k < 64; k++){
+                    for(k = 0; k < 64; k++)
                         min[k] = min[k] > ds[j][k] ? ds[j][k] : min[k];
-                    }
-
-                }
-            }
             //calculate connected subset in graph
-            for(j = 0; j < 64; j++){
-                if(min[j] == j) {
+            for(j = 0; j < 64; j++)
+                if(min[j] == j)
                     result++;
-                }
-            }
         }
         System.out.println(result);
     }
